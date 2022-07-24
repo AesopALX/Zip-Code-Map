faeab7f5631dd2a5f22a1fc226c24a8eb09f48f2
Andrew Samuelson <drewbaer@vt.edu>
2022-07-23 22:57:28 -0700

    First commit

---
+++ map.js
@@ -0,0 +1,16 @@
+const zips = [
+    '22301',
+    '00610',
+    '36273'
+];
+
+var map = L.map('map', {fullscreenControl: true}).setView([38.90, -77.09], 10);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+zips.forEach(zip => {
+    L.marker([latLongs[zip]['long'], latLongs[zip]['lat']]).addTo(map);
+})
